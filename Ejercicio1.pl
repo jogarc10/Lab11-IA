17ac79433242675e1e215d7a88dde89dbe3cdb9c
Jorge University <silversoldier9@hotmail.com>
2016-05-24 09:43:20 +0200

    Add files via upload

---
+++ Ejercicio1.pl
@@ -0,0 +1,15 @@
+verbo -> [V] {
+  atom_concat(Raiz, Terminacion, V),
+  es_raiz(Raiz, Infinitivo),
+  es_terminacion(Terminacion)
+}.
+
+% Diccionario
+es_raiz(habl)
+
+es_terminacion(o)
+es_terminacion(as)
+es_terminacion(a)
+es_terminacion(amos)
+es_terminacion(ais)
+es_terminacion(an)
